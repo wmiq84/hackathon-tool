8c96555a29f0e7e1e44898ca8dba4daf8e2c89e2
wmiq84 <michaelwang3489@gmail.com>
2024-11-23 18:40:12 -0800

    Build new issue page

--- app/tasks/page.tsx
+++ app/tasks/page.tsx
@@ -1,10 +1,17 @@
-import { Button } from '@radix-ui/themes'
-import React from 'react'
+import { Button } from '@radix-ui/themes';
+import Link from 'next/link';
+import React from 'react';
 
 const TasksPage = () => {
-  return (
-    <div><Button>New Issue</Button></div>
-  )
-}
+	return (
+		<div>
+			<Button>
+				<Link href="/tasks/new">
+				New Task
+        </Link>
+			</Button>
+		</div>
+	);
+};
 
-export default TasksPage
+export default TasksPage;
