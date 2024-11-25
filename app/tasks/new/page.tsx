8b989f4fb49082e66eabef44550852075ddf5f7a
wmiq84 <michaelwang3489@gmail.com>
2024-11-25 12:16:27 -0800

    Extract the error message component

--- app/tasks/new/page.tsx
+++ app/tasks/new/page.tsx
@@ -8,8 +8,10 @@ import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 import { zodResolver } from '@hookform/resolvers/zod';
+// reusable module
 import { createTaskSchema } from '@/app/validationSchemas';
 import { z } from 'zod';
+import ErrorMessage from '@/app/components/ErrorMessage';
 
 type IssueForm = z.infer<typeof createTaskSchema>;
 
@@ -46,7 +48,7 @@ const NewTaskPage = () => {
 				<TextField.Root>
 					<TextField.Input placeholder="Title" {...register('title')} />
 				</TextField.Root>
-				{errors.title && <Text color="red" as="p">{errors.title.message}</Text>}
+				<ErrorMessage>{errors.title?.message}</ErrorMessage>
 				<Controller
 					name="description"
 					control={control}
@@ -58,7 +60,11 @@ const NewTaskPage = () => {
 						/>
 					)}
 				/>
-				{errors.description && <Text color="red" as="p">{errors.description.message}</Text>}
+				<ErrorMessage>{errors.description?.message}</ErrorMessage>
+
+				{/* Above error message component replaces below */}
+				{/* {errors.description && <Text color="red" as="p">{errors.description.message}</Text>} */}
+
 				<Button>Submit New Task</Button>
 			</form>
 		</div>
