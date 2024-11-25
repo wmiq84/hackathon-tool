fb2ae731f1a92239de51b305a944c27f506d5fb2
wmiq84 <michaelwang3489@gmail.com>
2024-11-24 18:41:19 -0800

    Add markdown editor

--- app/tasks/new/page.tsx
+++ app/tasks/new/page.tsx
@@ -2,6 +2,8 @@
 
 import { Button, TextArea, TextField } from '@radix-ui/themes';
 import React from 'react';
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 
 const NewTaskPage = () => {
 	return (
@@ -9,7 +11,7 @@ const NewTaskPage = () => {
 			<TextField.Root>
 				<TextField.Input placeholder="Title" />
 			</TextField.Root>
-			<TextArea placeholder="Description" />
+			<SimpleMDE />
             <Button>Submit New Task</Button>
 		</div>
 	);
