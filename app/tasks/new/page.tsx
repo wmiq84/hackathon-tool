fd50767c1bfeb0f6b003cdfa8600edf12cf8bed1
wmiq84 <michaelwang3489@gmail.com>
2024-11-24 19:58:25 -0800

    Add error handling

--- app/tasks/new/page.tsx
+++ app/tasks/new/page.tsx
@@ -1,18 +1,57 @@
 'use client';
 
-import { Button, TextArea, TextField } from '@radix-ui/themes';
-import React from 'react';
-import SimpleMDE from "react-simplemde-editor";
-import "easymde/dist/easymde.min.css";
+import { Button, Callout, TextField } from '@radix-ui/themes';
+import React, { useState } from 'react';
+import SimpleMDE from 'react-simplemde-editor';
+import 'easymde/dist/easymde.min.css';
+import { useForm, Controller } from 'react-hook-form';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+
+interface IssueForm {
+	title: string;
+	description: string;
+}
 
 const NewTaskPage = () => {
+	const router = useRouter();
+	const { register, control, handleSubmit } = useForm<IssueForm>();
+	const [error, setError] = useState('');
+
 	return (
-		<div className="max-w-xl space-y-3">
-			<TextField.Root>
-				<TextField.Input placeholder="Title" />
-			</TextField.Root>
-			<SimpleMDE />
-            <Button>Submit New Task</Button>
+		<div className='max-w-xl '>
+			{error && (
+				<Callout.Root color="red" className='mb-5'>
+					<Callout.Text>{error}</Callout.Text>
+				</Callout.Root>
+			)}
+			<form
+				className="space-y-3"
+				onSubmit={handleSubmit(async (data) => {
+					try {
+						await axios.post('/api/tasks', data);
+						router.push('/tasks');
+					} catch (error) {
+						setError('An unexpected error occured.');
+					}
+				})}
+			>
+				<TextField.Root>
+					<TextField.Input placeholder="Title" {...register('title')} />
+				</TextField.Root>
+				<Controller
+					name="description"
+					control={control}
+					render={({ field }) => (
+						<SimpleMDE
+							{...field}
+							placeholder="Description"
+							onChange={(value) => field.onChange(value)}
+						/>
+					)}
+				/>
+				<Button>Submit New Task</Button>
+			</form>
 		</div>
 	);
 };
